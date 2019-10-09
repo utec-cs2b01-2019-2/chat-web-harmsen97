74610f19e6a68016b4d74be85ab88a023bf1b2bb
Jeronimo Harmsen <54420156+harmsen97@users.noreply.github.com>
2019-10-09 03:30:43 -0500

    Add files via upload

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -1,36 +1,34 @@
-function sendMessage(){
-    alert("Hola");
-}
-
-
 function get_all_users(){
-    console.log("Importando usuarios");
+    console.log("voy a traer todos los usuarios");
 
     $.getJSON("/users", function(data){
-    var i=0;
-
-    $.each(data, function(){
-    user_to = data[i]['id'];
-    e = '<li class="contact"><div class="wrap"> <span class="contact-status online"></span> <img src="static/images/'+data[i]['username'] +'.jpg" /><div class="meta"><p class="name">'+data[i]['username']+'</p><p class="preview">You just got LITT up, Mike.</p></div></div></li>';
-    i = i+1;
-    $("<li>",{html:e}).appendTo("#users");
+    var i = 0;
+    $.each(data,function(){
+        user_to = data[i]['id'];
+        e = '<div class = "alert" role="alert">';
+        e = e+ '<button type="button" class="btn btn-secondary btn-sm" id ="'+i+'" onclick="show_messages('+i+')">';
+        e = e+data[i]['username']+'</button>';
+        e = e+'</div>';i = i+1;
+        $("<div/>", {html:e}).appendTo("#users");
     });
+    });
+
+}
+
+function show_messages(i){                       
+    console.log(i);
+    $("#message").empty();
+    $.getJSON("/messages", function(data){
+        message_to=data[i]['id'];
+        e = '<div class = "alert" role="alert">';
+        e = e+ '<p>'+data[i]['content']+'</button>';
+        e = e+'</div>';
+        $("<div/>", {html:e}).appendTo("#message");
     });
 }
 
-//{"content": "hola", "id": 1, "sent_on": null, "user_from": null, "user_from_id": 4, "user_to": null, "user_to_id": 3}
-function get_all_messages(){
-    console.log("Trayendo mensajes");
-
-    $.getJSON("/messages",function(data){
-    var i=0;
-    content = data[i]['content'];
-    $.each(data,function(){
-
-    e = '<li class="sent"> <img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /> <p>'+data[i]['content']+'</p></li>'
-    i = i+1;
-    $("<li>",{html:e}).appendTo("#mensajes");
-    });
-    });
-
+function send_message(){    
+    var x=document.getElementById("txtMessage").value;
+    $("<div/>", {html:x}).appendTo("#message");  
+   
 }
---
+++ web/static/js/messages.js
@@ -0,0 +1,57 @@
+function getMessagesDevEx(){
+    var url = "http://127.0.0.1:8000/messages";
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            insertUrl: url,
+            updateUrl: url,
+            deleteUrl: url,
+            loadUrl: url,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+
+        editing: {
+            allowUpdating: true,    
+            allowDeleting: true,
+            allowAdding: true
+        },
+
+        remoteOperations: {
+             sorting: true,
+             paging: true
+        },
+
+        paging: {
+            pageSize: 12
+        },
+
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: true
+        }, {
+            dataField: "content"
+        }, {
+            dataField: "sent_on",
+        }, {
+            dataField: "user_from_id"
+        }, {
+            dataField: "user_to_id"
+        },{
+            dataField: "user_from"
+        },{
+            dataField: "user_to"
+        }]
+
+    }).dxDataGrid("instance");
+
+
+
+}
